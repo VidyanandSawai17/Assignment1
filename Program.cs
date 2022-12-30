cd1f68beaaed075d547249feeb0836026f759ce3
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2022-12-30 10:29:33 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,38 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            // Wthout using thrid variable
 
+            Console.WriteLine(" X =");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Y =");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+
+            X = X + Y;
+            Y = X - Y;
+            X = X - Y;
+
+            Console.WriteLine($"  X = {X}");
+            Console.WriteLine($"  Y = {Y}");
+
+
+
+            //using thrid variable
+
+            Console.WriteLine(" A =");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" B =");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int C = A;
+            A = B;
+            B = C;
+
+            Console.WriteLine($"  A = {A}");
+            Console.WriteLine($"  B = {B}");
 
         }
     }
